a8e1552a35d304f730e1c91d404b36efb14cb85c
hchgssarwyh <63221096+hchgssarwyh@users.noreply.github.com>
2023-06-08 04:00:05 +0300

    Create parses.py
    парсер для сбора датасета стихов

---
+++ parses.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+url = 'https://rustih.ru/vse-stixi-roberta-rozhdestvenskogo-na-odnoj-stranice/'
+f = open('D:rozjdestvenskiy.txt', 'w')
+r = requests.get(url)
+
+soup = bs(r.text, "html.parser")
+names = soup.find_all('li', class_ = 'listing-item')
+print(len(names))
+for name in names:
+    url_s = name.a.get('href')
+    r_s = requests.get(url_s)
+    soup_s = bs(r_s.text, "html.parser")
+    text = soup_s.find('div', class_ = 'entry-content poem-text')
+    strings = text.find_all('p')
+    s = ''
+    for i in strings:
+        for br in i('br'):
+            br.replace_with('')
+        if len(i.get_text())<1000:
+            s+=i.get_text()
+            s+='\n'
+            print(s)
+    
+    try:
+        f.write('\n')
+        f.write(s)
+    except:
+        pass
+f.close()
